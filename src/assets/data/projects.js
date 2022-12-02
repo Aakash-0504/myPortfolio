a99f801b32ad3af01efdf0edc361a39c56968e7b
Aakash <aakashvignesh20@gmail.com>
2022-12-02 14:46:15 +0530

    updated-push

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -2,7 +2,7 @@ export const ProjectData=[
     {
         id:1,
         title:"Tour Website",
-        about:"This is a Tour website with dynamic data fetching from a data file",
+        about:"This is a Tour website with dynamic data fetching from a data file.",
         tags:["React.js","Scss"],
         demo:'https://holiday-website-29dm6v516-aakash-0504.vercel.app/', 
         github:'https://github.com/Aakash-0504/holiday_website.git', 
@@ -12,7 +12,7 @@ export const ProjectData=[
     {
         id:2,
         title:"Netflix Clone",
-        about:"This is a Netflix clone and its dynamic data fetching from a api",
+        about:"This is a Netflix clone and its dynamic data fetching from a api.",
         tags:["React.js","Css"],
         demo:'https://netflix-clone-seven-roan.vercel.app/',
         github:'https://github.com/Aakash-0504/netflix_clone.git',
@@ -24,22 +24,14 @@ export const ProjectData=[
     {
         id:3,
         title:"Spices Website",
-        about:"This project is made with HTML,CSS and bootstrap",
+        about:"This project is made with HTML,CSS and bootstrap.",
         tags:["HTML","Css","Bootstrap"],
         demo:'https://spices-jpanrc4wf-aakash-0504.vercel.app/',
         github:'https://github.com/Aakash-0504/spices.git',
         image:  require('../../assets/images/spices.png'),
     },
 
-    {
-        id:4,
-        title:"Portfolio-Project",
-        about:"This is my portfolio website and its dynamic and each section is in a seperate component and fetchs data using react hooks",
-        tags:["React.js","Css"],
-        demo:'https://my-portfolio-cyan-one.vercel.app/',
-        github:'https://github.com/Aakash-0504/myPortfolio.git',
-        image: require('../../assets/images/portfolio.png'),
-    },
+   
 
 
     
--- src/components/body/about/index.js
+++ src/components/body/about/index.js
@@ -7,7 +7,7 @@ function About() {
     <div className='about'>
       <div className="about-top">
         <div className="about-info">
-          Hi This is <br /><span className='info-name'>Aakash</span><br />Iam a versatile Frontend Developer<br/>
+         <span className='info-name'>Aakash</span>  Here,<br/>Iam a versatile Frontend Developer<br/>
           who is  Responsible, creative, and hardworking. <br/>I have a passion for learning new technologies and<br/>I am a quick learner and I am always ready to take on new challenges.
         </div>
         <div className="about-photo">
